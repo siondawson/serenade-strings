a7aa03dbb9a94cfa8e4c59e21e9c08b65591887d
siondawson <siondawson91@gmail.com>
2023-04-14 19:45:24 +0000

    added console.log in js to look for bug

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,8 +1,9 @@
 function sendMail(contactForm) {
+    console.log("Hi");
     emailjs.send("service_83yeifc", "KensingtonStrings", {
         "from_name": contactForm.name.value,
         "from_number": contactForm.contact-number.value,
-        "from_email": contactForm.emailaddress.value,
+        "from_email": contactForm.email.value,
         "message": contactForm.message.value
     })
     .then(

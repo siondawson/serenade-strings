5f8ff8e6e28732a9a9426684d3474ecd5a61e27e
siondawson <siondawson91@gmail.com>
2024-01-08 16:49:42 +0000

    added faq.html page and connected to json file

---
+++ assets/js/faq.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const faqsContainer = document.getElementById('faqs');
+
+    fetch('assets/json/faq.json')
+        .then(response => response.json())
+        .then(data => {
+            for (const [question, answer] of Object.entries(data)) {
+                const faqItem = document.createElement('div');
+                faqItem.innerHTML = `
+                    <h3>${question}</h3>
+                    <p>${answer}</p>
+                `;
+                faqsContainer.appendChild(faqItem);
+            }
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
+
+

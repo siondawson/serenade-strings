4632b568ebcb0c9bea7d131db4defd685757d2c9
siondawson <siondawson91@gmail.com>
2024-01-12 14:57:41 +0000

    fixed bugs in sendEmail function and contact form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,10 +1,12 @@
-function sendEmail() {
+function sendEmail(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
     var params = {
         name: document.getElementById("name").value,
         contact_number: document.getElementById("contact-number").value,
         email: document.getElementById("email").value,
         message: document.getElementById("message").value,
-        where_did_you_hear: document.querySelector('input[name="where-did-you-hear"]:checked').value,
+        where_did_you_hear: document.querySelector('input[name="where-did-you-hear"]:checked') ? document.querySelector('input[name="where-did-you-hear"]:checked').value : '',
     };
 
     const serviceID = "service_qfwwcsl";
@@ -23,5 +25,8 @@ function sendEmail() {
                 alert("Your message sent successfully");
             }
         )
-        .catch((err) => console.log(err));
+        .catch((err) => {
+            console.error(err);
+            alert("There was an error sending your message. Please try again later.");
+        });
 }
